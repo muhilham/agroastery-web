2b2c7d1cd80a10aacc6d08f1b36d6dc35f08168a
Muhammad Ilham <muhilham.dev@gmail.com>
2026-04-09 14:31:26 +0700

    chore: delete dead legacy category and product nanostores

--- lib/stores/category.ts
+++
@@ -1,6 +0,0 @@
-import { CATEGORY } from "@/constant/category";
-import { TCategory } from "@/types/categories";
-import { atom } from "nanostores";
-
-export const $categories = atom<TCategory[]>(CATEGORY);
-export const $selectedCategoryId = atom<string | null>(null);
