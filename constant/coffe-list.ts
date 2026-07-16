296f9e3f122ac7abcda394208ed8247e01e1bf7c
Muhammad Ilham <muhilham.dev@gmail.com>
2026-07-16 14:51:19 +0700

    fix(links): update homepage coffee product links to correct paths

--- constant/coffe-list.ts
+++ constant/coffe-list.ts
@@ -6,27 +6,27 @@ export const coffeList: I_ContentInterface[] = [
     subtitle:
       "Arabica semi-washed dari 1600 mdpl, ideal untuk espresso dan latte dengan body ringan, sweetness tinggi dan acidity rendah.",
     image: "/assets/coffe/blend-gayo.png",
-    href: "/product/biji-kopi-standard-gayo-full-arabica-kopi-arabika-freshroasted",
+    href: "/product/biji-kopi-standard-gayo-full-arabica",
   },
   {
     title: "BLEND KOPI SUSU EKONOMIS",
     subtitle:
       "Campuran 50% Arabica dan 50% Robusta, ideal untuk es kopi susu dengan rasa seimbang dan harga ekonomis.",
     image: "/assets/coffe/blend-kopi-susu.png",
-    href: "/product/grosir-min-10kg-biji-kopi-blend-5050-kopi-susu-ekonomis-1-kg",
+    href: "/product/biji-kopi-blend-5050-kopi-susu-ekonomis",
   },
   {
     title: "HOUSE BLEND ARABICA & ROBUSTA",
     subtitle:
       "Espresso blend dengan karakter cokelat manis dan sweetness kuat, cocok untuk kopi susu hingga long black.",
     image: "/assets/coffe/blend-house.png",
-    href: "product/grosir-min-10kg-house-blend-espresso---arabica-fine-robusta-1-kg-es46-blend-1kg",
+    href: "/product/house-blend-espresso-arabica-fine-robusta-es46",
   },
   {
     title: "SOLOK SELATAN FULL ARABICA",
     subtitle:
       "Arabica berkualitas dengan sangrai optimal, pas untuk espresso, americano, long black, latte, dan cappuccino.",
     image: "/assets/coffe/blend-solok.png",
-    href: "/product/grosir-min-10kg-biji-kopi-full-arabica-kopi-susu-ekonomis-1-kg ",
+    href: "/product/biji-kopi-standard-solok-selatan-full-arabica",
   },
 ];
