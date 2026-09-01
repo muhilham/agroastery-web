f53f42817537bd6d5db1a61ee7bca49651c7b35e
Muhammad Ilham <muhilham.dev@gmail.com>
2026-09-01 13:52:47 +0700

    feat: add floating WhatsApp button on all pages (#114)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { WebVitals } from "@/components/WebVitals";
+import { FloatingWhatsApp } from "@/components/FloatingWhatsApp";
 import "./globals.css";
 import "../style/embla.css";
 
@@ -48,6 +49,7 @@ export default function RootLayout({
       <body className="bg-background" style={{ fontFamily: "Montserrat, system-ui, sans-serif" }}>
         {children}
         <WebVitals />
+        <FloatingWhatsApp phone={process.env.ORIGIN_CONTACT_PHONE?.replace(/^\+/, "") ?? "628****2726"} />
         {gaId && <GoogleAnalytics gaId={gaId} />}
       </body>
     </html>
